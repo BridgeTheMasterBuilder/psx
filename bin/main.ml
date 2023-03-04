d8532e8a6e79504e202a59dea1924685cf5f8a72
Master Builder <gylfi@stebbi.is>
2023-03-04 23:10:03 +0000

    Continue migrating tomorrow

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,10 @@
-let () = print_endline "Hello, World!"
+open Cmdliner
+
+let parse_args () =
+  (* let rom = Arg.(required & pos 0 (some string) None & info [] ~docv:"ISO") in *)
+  let info = Cmd.info "psx" in
+  (* let cmd = Cmd.v info Term.(const Psx.main $ iso) in *)
+  let cmd = Cmd.v info Term.(const Psx.main $ const ()) in
+  exit (Cmd.eval cmd)
+
+let () = parse_args ()
